ada36461071314c6c6dde8965bc790646a214e42
andreicismas <andrei_cismas@yahoo.it>
2021-04-14 23:39:40 +0200

    fin palidromo

---
+++ PRIMO ESS/script.js
@@ -0,0 +1,36 @@
+// chiediamo all utente da inserire una parola
+var parolaInserita = prompt("inserisci una parola")
+
+
+
+// validiamo che il utente non srive numeri
+var parolaInNumero = parseInt(parolaInserita);
+
+// condizioni prompt
+if (Number.isNaN(parolaInNumero)) {
+  console.log(parolaInserita)
+
+} else {
+  alert("i numeri non sono validi")
+}
+
+// variabile /function
+var loGirata = controloParolePalindroma(parolaInserita);
+
+// condizioni function
+if (parolaInserita == loGirata) {
+  console.log("la parola"+" "+ parolaInserita.toLowerCase()+" "+"  e palidroma");
+}
+else {
+  console.log("la parola"+" "+ parolaInserita.toLowerCase()+" "+"  NON e palidroma");
+}
+
+// function parola girata
+function controloParolePalindroma(controlo) {
+  var controloParola = "";
+  for (var i = controlo.length - 1; i >= 0; i--) {
+    controloParola += controlo[i];
+  }
+  return controloParola;
+}
+
