d4b047b419ec44cbd2705fe9654c63fcd72effdf
andreicismas <andrei_cismas@yahoo.it>
2021-04-14 16:08:03 +0200

    start

---
+++ PRIMO ESS/SECCONDO ESS/sec.script.js
@@ -0,0 +1,10 @@
+var pariDispari = prompt("segli tra pari o dispari")
+
+if (pariDispari !== "pari" && pariDispari !== "dispari") {
+    alert("devi inserire....:'pari' o 'dispari'")
+}
+console.log(pariDispari)
+
+function risultatoparola() {
+
+}
