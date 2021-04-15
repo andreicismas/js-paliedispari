da716445be83142515c9197ebd19145a235a72cb
andreicismas <andrei_cismas@yahoo.it>
2021-04-15 11:19:08 +0200

    last push

--- PRIMO ESS/SECCONDO ESS/sec.script.js
+++ PRIMO ESS/SECCONDO ESS/sec.script.js
@@ -6,34 +6,34 @@ var numeriValidi = parseInt(pariDispari);
 if (pariDispari !== "pari" && pariDispari !== "dispari") {
     alert("devi inserire....:'pari' o 'dispari'");
 } else {
-    console.log("il umano ha scritto "+" "+pariDispari)
+    console.log("il umano ha scritto " + " " + pariDispari)
+
+    // variabile i numeri inseriti dal utente
+    var numeriDaInserire = parseInt(prompt("inserisci numeri da 1-5"));
+
+
+    // condizioni per validare i numeri 
+    if (numeriDaInserire > 5 || numeriDaInserire < 1) {
+        alert("solo numeri da 1-5!")
+    } else {
+        console.log(numeriDaInserire)
+    }
+    // function math per numeri random
+    function getRandomInt(numMax) {
+        return Math.floor(Math.random(5) * numMax) + 1;
+    }
+
+    // risultato  addizione umano e random 
+    var risultato = numeriDaInserire + getRandomInt(5)
+    console.log(risultato)
+
+
+    //  condizione chi vince
+    if (risultato % 2) {
+        console.log("il pari vince")
+
+    } else {
+        console.log("dispari vince")
+    }
 }
 
-
-// variabile i numeri inseriti dal utente
-var numeriDaInserire = parseInt(prompt("inserisci numeri da 1-5"));
-
-
-// condizioni per validare i numeri 
-if (numeriDaInserire > 5 || numeriDaInserire < 1) {
-    alert("solo numeri da 1-5!")
-} else {
-    console.log(numeriDaInserire)
-}
-// function math per numeri random
-function getRandomInt(numMax) {
-    return Math.floor(Math.random(5) * numMax) + 1;
-}
-
-// risultato  addizione umano e random 
-var risultato = numeriDaInserire + getRandomInt(5) 
-console.log(risultato)
-
-
-//  condizione chi vince
-if(risultato  % 2){
-    console.log("il pari vince")
-
-}else {
-    console.log("dispari vince")
-}
