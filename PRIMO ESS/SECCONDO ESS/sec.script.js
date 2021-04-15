e0599023d322d2bc02f569195784c473855ca886
andreicismas <andrei_cismas@yahoo.it>
2021-04-15 07:30:27 +0200

    lavoro sulla funzione

--- PRIMO ESS/SECCONDO ESS/sec.script.js
+++ PRIMO ESS/SECCONDO ESS/sec.script.js
@@ -1,10 +1,19 @@
 var pariDispari = prompt("segli tra pari o dispari")
+var numeriValidi=parseInt(pariDispari);
 
-if (pariDispari !== "pari" && pariDispari !== "dispari") {
-    alert("devi inserire....:'pari' o 'dispari'")
+if (pariDispari !== "pari" && pariDispari !== "dispari"){
+    alert("devi inserire....:'pari' o 'dispari'");
+}else{
+    console.log(pariDispari)
 }
-console.log(pariDispari)
 
-function risultatoparola() {
+var numeriDaInserire =prompt("inserisci numeri da 1-5");
 
+if(numeriDaInserire > 5 || numeriDaInserire < 1){
+    alert("inserisci numeri!")
+}else {
+    console.log(numeriDaInserire)
 }
+
+function mateFunction()
+
